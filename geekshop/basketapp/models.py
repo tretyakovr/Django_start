e208aa36ffcc2aef9d55c15e45fec77c7025ef6f
rtretyakov <rtretyakov@outlook.com>
2021-10-12 07:36:00 +1000

    OptTools Lesson04_RTretyakov

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -3,7 +3,17 @@ from django.conf import settings
 from mainapp.models import Products
 
 
+class BasketQuerySet(models.QuerySet):
+
+   def delete(self, *args, **kwargs):
+       for object in self:
+           object.product.quantity += object.quantity
+           object.product.save()
+       super(BasketQuerySet, self).delete(*args, **kwargs)
+
+
 class Basket(models.Model):
+    objects = BasketQuerySet.as_manager()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket')
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
@@ -13,24 +23,34 @@ class Basket(models.Model):
         db_table = 'Basket'
         verbose_name = 'Корзина с товарами'
 
+    @property
+    def product_cost(self):
+        # return cost of all products this type
+        return self.product.price * self.quantity
 
-@property
-def product_cost(self):
-    # return cost of all products this type
-    return self.product.price * self.quantity
+    @property
+    def total_quantity(self):
+        # return total quantity for user
+        _items = Basket.objects.filter(user=self.user)
+        _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
+        return _totalquantity
 
+    @property
+    def total_cost(self):
+        # return total cost for user
+        _items = Basket.objects.filter(user=self.user)
+        _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
+        return _totalcost
 
-@property
-def total_quantity(self):
-    # return total quantity for user
-    _items = Basket.objects.filter(user=self.user)
-    _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
-    return _totalquantity
+    def delete(self):
+        self.product.quantity += self.quantity
+        self.product.save()
+        super(self.__class__, self).delete()
 
-
-@property
-def total_cost(self):
-    # return total cost for user
-    _items = Basket.objects.filter(user=self.user)
-    _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
-    return _totalcost
+    def save(self, *args, **kwargs):
+        if self.pk:
+            self.product.quantity -= self.quantity - self.__class__.get_item(self.pk).quantity
+        else:
+            self.product.quantity -= self.quantity
+        self.product.save()
+        super(self.__class__, self).save(*args, **kwargs)
